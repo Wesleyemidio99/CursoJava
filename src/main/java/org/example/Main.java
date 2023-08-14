333a826b5cab300322a9e43ca94919858c888718
wesley <wesleyemidio99@hotmail.com>
2023-08-14 15:11:28 -0300

    Inicialização

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+/*Fazer um programa para ler os dados de um produto em estoque (nome,preço e quantidade em estoque). Em seguida:
+* Mostrar os dados dos produtos(nome,preço, quantidade em estoque, valor total no estoque).
+* Realizar uma entrada no estoque e mostrar novamente os dados do produto.
+* Realizar uma saída no estoque e mostrar novamente os dados do produto.*/
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data:");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.name + ", "+product.price + ", "+product.quantity);
+
+
+        sc.close();
+
+    }
+}
